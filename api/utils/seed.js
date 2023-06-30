fd8b0c6b64719e3af863674a29882ae4538f114e
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-30 16:02:54 -0600

    error handling seed

--- api/utils/seed.js
+++ api/utils/seed.js
@@ -3,28 +3,32 @@ const db = require("../db");
 const fakeData = require("./fakeData.json");
 
 async function runSeed() {
-  await db.sync({ force: true });
-  await db.sync({ force: false });
+  try {
+    await db.sync({ force: true });
+    await db.sync({ force: false });
 
-  await User.create({
-    first_name: "John",
-    last_name: "Doe",
-    email: "admin@mail.com",
-    password: "password",
-    is_admin: true,
-  });
+    await User.create({
+      first_name: "John",
+      last_name: "Doe",
+      email: "admin@mail.com",
+      password: "password",
+      is_admin: true,
+    });
 
-  await User.create({
-    first_name: "Jane",
-    last_name: "Doe",
-    email: "demo@mail.com",
-    password: "password",
-    is_admin: false,
-  });
+    await User.create({
+      first_name: "Jane",
+      last_name: "Doe",
+      email: "demo@mail.com",
+      password: "password",
+      is_admin: false,
+    });
 
-  await Shirt_Model.bulkCreate(fakeData);
+    await Shirt_Model.bulkCreate(fakeData);
+
+    console.log("Seed completed successfully.");
+  } catch (error) {
+    console.error("An error occurred during seed execution:", error);
+  }
 }
 
-runSeed().catch((error) => {
-  console.error("An error occurred during seed execution:", error);
-});
+runSeed();
