4232700cab7f777782dec59971aa75e1471e307b
garcelomarcia <marcelogarcia91@gmail.com>
2023-06-30 15:27:19 -0600

    fixed postgresql

--- api/db/index.js
+++ api/db/index.js
@@ -1,14 +1,19 @@
-const Sequelize = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // This line allows you to connect to a database with a self-signed certificate
+// Create a new Sequelize instance
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL ||
+    "postgresql://myuser:mypassword@187.162.118.101:5432/ecommerce",
+  {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // This line allows you to connect to a database with a self-signed certificate
+      },
     },
-  },
-});
+  }
+);
 
 module.exports = sequelize;
