d858409a1ba07d0bdd4c0f0782847a33f9307f03
garcelomarcia <marcelogarcia91@gmail.com>
2023-07-04 09:11:33 -0600

    added condition to sendMail route

--- api/controllers/sendMailController.js
+++ api/controllers/sendMailController.js
@@ -8,13 +8,15 @@ exports.sendMail = async (req, res) => {
     const foundCartWithItems = await Cart.findOne({
       where: { userId: id, state: "active" },
     });
-    await transporter.sendMail({
-      from: "CustomShirt@empresa.com",
-      to: user.email,
-      subject: "Aqui tienes tu detalle de la compra!!!!",
-      text: "Confirma tu compra",
-      html: `<h1>Hola ${user.first_name}!!</h1> <h2>Gracias por tu compra</h2> <h3>Total pagado: ${foundCartWithItems.totalCost}</h3>`,
-    });
+    if (user.email !== "demo@mail.com") {
+      await transporter.sendMail({
+        from: "CustomShirt@empresa.com",
+        to: user.email,
+        subject: "Aqui tienes tu detalle de la compra!!!!",
+        text: "Confirma tu compra",
+        html: `<h1>Hola ${user.first_name}!!</h1> <h2>Gracias por tu compra</h2> <h3>Total pagado: ${foundCartWithItems.totalCost}</h3>`,
+      });
+    }
     res.send("Your order is completed!");
   } catch (err) {
     console.log(err, "error finding active cart");
