066cdc8c0f956ff1688a23f10fec7ed22d894aa7
garcelomarcia <marcelogarcia91@gmail.com>
2023-08-18 13:50:04 -0600

    added ssl configuration to sequelize

--- api/db/index.js
+++ api/db/index.js
@@ -8,10 +8,16 @@ const dbUser = process.env.DB_USER;
 const dbHost = process.env.DB_HOST;
 const dbPassword = process.env.DB_PASSWORD;
 
-const db = new Sequelize(dbName, dbUser, dbPassword, {
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true, // Require SSL connection
+      rejectUnauthorized: false // Accept self-signed certificates
+    }
+  }
 });
 
 module.exports = db;
